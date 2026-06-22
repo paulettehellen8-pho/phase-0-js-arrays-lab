2d0d592907ff6476566a441d4412530d55404a3b
Paulette Hellen Obimbo <paulettehellen8@gmail.com>
2026-06-22 15:22:10 +0300

    Added functions and an array

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,5 +1,31 @@
 // Write your code here
+const products = ["Laptop", "Phone", "Headphones", "Monitor"];
+console.log(products)
 
+function logFirstProduct() {
+  console.log(products[0]);
+}
+logFirstProduct();
+
+function addProduct(productName) {
+  products.push(productName);
+}
+addProduct("Mouse");
+console.log(products);
+
+function updateProductName(index, newName) {
+  let oldName = products[index];
+  products[index] = newName;
+}
+updateProductName(1, "Smartphone");
+console.log(products);
+
+function removeLastProduct() {
+  products.pop();
+
+}
+removeLastProduct();
+console.log(products);
 
 
 // Export the necessary parts for testing
